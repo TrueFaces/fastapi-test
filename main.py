244bc5bd7752970d6824f37f5f53cb2abca1492d
Armando Bueno <abueno@facephi.com>
2023-03-25 19:32:25 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"hello": "world"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
