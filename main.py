8b2ff96a33f416db3c3d40e296625178dc1e5cdc
Armando Bueno <abueno@facephi.com>
2023-03-25 21:27:26 +0100

    Added uploadfile with multipart

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 
 app = FastAPI()
+
 
 @app.get("/")
 async def root():
     return {"hello": "world"}
 
+
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
     return {"item_id": item_id}
+
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: bytes = File(...)):
+    return {"file_size": len(file)}
