c2db9c29f3f9dc665cda7bbc59c30efa043e1492
Juan Ignacio <sr.jifernandez@gmail.com>
2023-03-28 20:02:11 +0200

    Test automatic deployment. New function to write object in bucket.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Code from : https://github.com/tiangolo/fastapi/issues/364
 
 ## Main
+from google.cloud import storage
 from fastapi import FastAPI, File, UploadFile
 import uvicorn, os
 ## Security Basic Auth
@@ -35,9 +36,27 @@ async def get_redoc_documentation():
     return get_redoc_html(openapi_url="/openapi.json", title="docs")
     
 @app.post("/uploadfile/")
-async def create_upload_file(file: bytes = File(...)):
-    return {"file_size": len(file)}
+async def create_upload_file(file: UploadFile):
+    # upload_file_to_bucket("", "data/user/1/" + file.filename, file)
+    return {"file_size": len(file), "filename": file.filename}
     
 ## RUN!!!
 if __name__ == "__main__":
     uvicorn.run(app, port=int(os.environ.get("PORT", 8000)), host="0.0.0.0")
+
+def upload_file_to_bucket(bucket_name, blob_name, file):
+    """Write and read a blob from GCS using file-like IO"""
+    # The ID of your GCS bucket
+    # bucket_name = "your-bucket-name"
+
+    # The ID of your new GCS object
+    # blob_name = "storage-object-name"
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+
+    # Mode can be specified as wb/rb for bytes mode.
+    # See: https://docs.python.org/3/library/io.html
+    with blob.open("w") as f:
+        f.write(file)
