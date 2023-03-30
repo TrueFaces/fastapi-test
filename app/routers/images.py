17d38f4de4128172cae5c8fa7e33f0ad8e64899f
Juan Ignacio <sr.jifernandez@gmail.com>
2023-03-30 21:35:08 +0200

    Primera version del login

---
+++ app/routers/images.py
@@ -0,0 +1,59 @@
+from fastapi import APIRouter, Depends, HTTPException, File, UploadFile
+from google.cloud import storage
+
+router = APIRouter(
+    prefix="/images",
+    tags=["images"],
+)
+
+
+fake_items_db = {"plumbus": {"name": "Plumbus"}, "gun": {"name": "Portal Gun"}}
+
+
+
+@router.get("/")
+async def read_items():
+    return fake_items_db
+
+
+@router.get("/{item_id}")
+async def read_item(item_id: str):
+    if item_id not in fake_items_db:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return {"name": fake_items_db[item_id]["name"], "item_id": item_id}
+
+
+# @router.put(
+#     "/{item_id}",
+#     tags=["custom"],
+#     responses={403: {"description": "Operation forbidden"}},
+# )
+# async def update_item(item_id: str):
+#     if item_id != "plumbus":
+#         raise HTTPException(
+#             status_code=403, detail="You can only update the item: plumbus"
+#         )
+#     return {"item_id": item_id, "name": "The great Plumbus"}
+
+
+@router.post("/upload/")
+async def create_upload_file(file: UploadFile):
+    # upload_file_to_bucket("", "data/user/1/" + file.filename, file)
+    return {"file_size": file.size, "filename": file.filename}
+    
+def upload_file_to_bucket(bucket_name, blob_name, file):
+    """Write and read a blob from GCS using file-like IO"""
+    # The ID of your GCS bucket
+    # bucket_name = "your-bucket-name"
+
+    # The ID of your new GCS object
+    # blob_name = "storage-object-name"
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+
+    # Mode can be specified as wb/rb for bytes mode.
+    # See: https://docs.python.org/3/library/io.html
+    with blob.open("w") as f:
+        f.write(file)
--- main.py
+++ main.py
@@ -1,32 +1,27 @@
-# Code from : https://github.com/tiangolo/fastapi/issues/364
-
-## Main
-from google.cloud import storage
-from fastapi import FastAPI, File, UploadFile
+from fastapi import Depends, FastAPI,  File, UploadFile
 import uvicorn, os
-## Security Basic Auth
-import secrets
-from fastapi import Depends, FastAPI, HTTPException, status
-from fastapi.security import HTTPBasic, HTTPBasicCredentials
+
 ## Add Doc Security
 from fastapi.openapi.docs import get_redoc_html, get_swagger_ui_html
 from fastapi.openapi.utils import get_openapi
+from fastapi.security import OAuth2PasswordBearer
 
-## Initial App
-app = FastAPI(
-    title="FastAPI",
-    version="0.1.0",
-    docs_url=None,
-    redoc_url=None,
-    openapi_url = None,
-)
+from app.internal import admin
+from app.routers import images, users, auth
 
-## Create Auth Doc
-@app.get("/openapi.json", include_in_schema=False)
-async def openapi():
-    return get_openapi(title=app.title, version=app.version, routes=app.routes)
 
-@app.route('/')
+app = FastAPI()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+app.include_router(auth.router)
+app.include_router(users.router, dependencies=[Depends(oauth2_scheme)])
+app.include_router(images.router, dependencies=[Depends(oauth2_scheme)])
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to TrueFaces"}
+
 @app.get("/docs", include_in_schema=False)
 async def get_swagger_documentation():
     return get_swagger_ui_html(openapi_url="/openapi.json", title="docs")
@@ -34,28 +29,6 @@ async def get_swagger_documentation():
 @app.get("/redoc", include_in_schema=False)
 async def get_redoc_documentation():
     return get_redoc_html(openapi_url="/openapi.json", title="docs")
-    
-@app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile):
-    # upload_file_to_bucket("", "data/user/1/" + file.filename, file)
-    return {"file_size": file.size, "filename": file.filename}
-    
-def upload_file_to_bucket(bucket_name, blob_name, file):
-    """Write and read a blob from GCS using file-like IO"""
-    # The ID of your GCS bucket
-    # bucket_name = "your-bucket-name"
-
-    # The ID of your new GCS object
-    # blob_name = "storage-object-name"
-
-    storage_client = storage.Client()
-    bucket = storage_client.bucket(bucket_name)
-    blob = bucket.blob(blob_name)
-
-    # Mode can be specified as wb/rb for bytes mode.
-    # See: https://docs.python.org/3/library/io.html
-    with blob.open("w") as f:
-        f.write(file)
 
 ## RUN!!!
 if __name__ == "__main__":
