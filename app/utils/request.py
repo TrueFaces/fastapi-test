cba6333cb68186c9bc4bb4c84ef4f7d72fbfd3a5
Juan Ignacio <sr.jifernandez@gmail.com>
2023-04-19 21:15:44 +0200

    Se fija a https

--- app/utils/request.py
+++ app/utils/request.py
@@ -1,4 +1,5 @@
 from fastapi import Request
 
 def get_domain(request:Request):
-    return request.url.scheme + "://" + request.url.hostname
+    #return request.url.scheme + "://" + request.url.hostname
+    return "https://" + request.url.hostname
